e13fb78a61c8e67ba44109f1c0dd7c8d8a70ed6c
lnik7397-lab <lnik7397@gmail.com>
2026-06-18 20:15:17 +0500

    Add files via upload

--- flora_salon/supabase.js
+++ flora_salon/supabase.js
@@ -1,12 +1,9 @@
 
 
-const SUPABASE_URL = 'https://mkmpljmpdizbhokjmkow.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rbXBsam1wZGl6Ymhva2pta293Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NTQ5NDgsImV4cCI6MjA5NjMzMDk0OH0.TrMmfS9DzXAO9IPYzLpMIR_LCEHFCn6PpUcCCEURyTY';
+const supabaseUrl = 'https://mkmpljmpdizbhokjmkow.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rbXBsam1wZGl6Ymhva2pta293Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NTQ5NDgsImV4cCI6MjA5NjMzMDk0OH0.TrMmfS9DzXAO9IPYzLpMIR_LCEHFCn6PpUcCCEURyTY';
 
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-
-window.supabase = supabaseClient;
-
-console.log('Supabase инициализирован:', window.supabase);
+console.log('✅ Supabase инициализирован глобально');
